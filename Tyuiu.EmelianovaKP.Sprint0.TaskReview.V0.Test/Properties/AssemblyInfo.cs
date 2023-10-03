b24c9e58c47045eef426868392b1788f66cd8b1a
fivifri <emelianova.kp@phystech.edu>
2023-10-03 10:27:42 +0500

    Add project files.

---
+++ Tyuiu.EmelianovaKP.Sprint0.TaskReview.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.EmelianovaKP.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.EmelianovaKP.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3996bd7-a061-49de-90f0-95e1cac7ec2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.EmelianovaKP.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.EmelianovaKP.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.EmelianovaKP.Sprint0.TaskReview.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнил: Емельянова К. П. | ПКТб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: TaskReview                                                        *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Емельянова К. П. | ПКТб-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Cоздать метод с именами и параметрами: Calc(int x, int y, int z),       *");
+            Console.WriteLine("* который вычисляет следующее выражение (x+y+z)*5 и возвращает            *");
+            Console.WriteLine("* целочисленное значение.                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y, z;
+
+            Console.WriteLine("Введите x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите z: ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Результат: " + DataService.Calc(x, y, z));
+
+            Console.ReadKey();
+        }
+    }
+}
